ef58b6533a24ff2183f5c1736b8ba7b7ceb4877e
minko124 <dalukwi@gmail.com>
2026-07-21 13:19:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Portfolio Start!");
+
+const fades = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+
+    const trigger = window.innerHeight * 0.85;
+
+    fades.forEach(item => {
+
+        const top = item.getBoundingClientRect().top;
+
+        if(top < trigger){
+            item.classList.add("show");
+        }
+
+    });
+
+});
