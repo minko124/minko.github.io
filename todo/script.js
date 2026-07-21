e3d61fb879c65c93f0c0ad3f35a04bfa107d68ac
minko124 <dalukwi@gmail.com>
2026-07-21 18:48:31 +0900

    Update script.js

--- todo/script.js
+++ todo/script.js
@@ -1,1 +1,28 @@
+const input = document.getElementById("taskInput");
+const list = document.getElementById("taskList");
 
+function addTask() {
+
+    const text = input.value.trim();
+
+    if (text === "") {
+        alert("やることを入力してください");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        <span>${text}</span>
+        <button class="delete">削除</button>
+    `;
+
+    li.querySelector(".delete").addEventListener("click", function () {
+        li.remove();
+    });
+
+    list.appendChild(li);
+
+    input.value = "";
+    input.focus();
+}
